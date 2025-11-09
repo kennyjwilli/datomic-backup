a0405367d6dcca64798e20fc6fe9428a1db03cad
Kenny Williams <kwill@hey.com>
2025-11-09 10:51:21 -0800

    add incremental-restore-with-composite-tuples-test

--- test/dev/kwill/datomic_backup_test.clj
+++ test/dev/kwill/datomic_backup_test.clj
@@ -104,3 +104,76 @@
                                   :student/email]}]
               [:school/id 1]))))))
 
+(deftest incremental-restore-with-composite-tuples-test
+  (with-open [ctx (testh/test-ctx {:dbs [:source-conn :dest-conn :state-conn]})]
+    (testing "Incremental restore with composite tuple attributes"
+      ;; Phase 1: Initial restore with composite tuple data
+      (testh/test-data! (:source-conn ctx))
+      (let [result (backup/incremental-restore
+                     {:source-conn (:source-conn ctx)
+                      :dest-conn   (:dest-conn ctx)
+                      :state-conn  (:state-conn ctx)})
+            session-id (:session-id result)]
+
+        (is (= :initial (:status result))
+          "First restore should have :initial status")
+
+        ;; Verify composite tuple schema attribute ID is in mappings
+        (let [source-db (d/db (:source-conn ctx))
+              composite-tuple-attr-id (ffirst (d/q '[:find ?e
+                                                      :where [?e :db/ident :semester/year+season]]
+                                                 source-db))
+              old-id->new-id (:old-id->new-id result)]
+          (is (contains? old-id->new-id composite-tuple-attr-id)
+            "Composite tuple schema attribute ID should be in mappings"))
+
+        ;; Verify initial composite tuple values exist
+        (let [dest-db (d/db (:dest-conn ctx))
+              semester (d/pull dest-db
+                         [:semester/year :semester/season :semester/year+season]
+                         [:semester/year+season [2018 :fall]])]
+          (is (= 2018 (:semester/year semester)))
+          (is (= :fall (:semester/season semester)))
+          (is (= [2018 :fall] (:semester/year+season semester))
+            "Composite tuple value should be correct after initial restore"))
+
+        ;; Phase 2: Add more data with composite tuples
+        (d/transact (:source-conn ctx)
+          {:tx-data [{:semester/year   2019
+                      :semester/season :spring}
+                     {:semester/year   2020
+                      :semester/season :summer}]})
+
+        ;; Phase 3: Second incremental restore
+        (let [result2 (backup/incremental-restore
+                        {:source-conn (:source-conn ctx)
+                         :dest-conn   (:dest-conn ctx)
+                         :state-conn  (:state-conn ctx)})]
+
+          (is (= :incremental (:status result2))
+            "Second restore should have :incremental status")
+          (is (= session-id (:session-id result2))
+            "Should reuse same session")
+
+          ;; Verify all composite tuple values are correct
+          (let [dest-db (d/db (:dest-conn ctx))
+                semester-2018 (d/pull dest-db
+                                [:semester/year :semester/season :semester/year+season]
+                                [:semester/year+season [2018 :fall]])
+                semester-2019 (d/pull dest-db
+                                [:semester/year :semester/season :semester/year+season]
+                                [:semester/year+season [2019 :spring]])
+                semester-2020 (d/pull dest-db
+                                [:semester/year :semester/season :semester/year+season]
+                                [:semester/year+season [2020 :summer]])]
+
+            ;; Verify original data still correct
+            (is (= [2018 :fall] (:semester/year+season semester-2018))
+              "Original composite tuple should remain correct")
+
+            ;; Verify new data from incremental restore
+            (is (= [2019 :spring] (:semester/year+season semester-2019))
+              "New composite tuple from incremental restore should be correct")
+            (is (= [2020 :summer] (:semester/year+season semester-2020))
+              "Second new composite tuple should be correct")))))))
+
